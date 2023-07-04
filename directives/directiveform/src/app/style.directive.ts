8e9c9fcc0c38021f89f011511ae53fbd518f6481
Ku Nal <104050919+Kunalyadava@users.noreply.github.com>
2023-07-04 23:50:03 +0530

    added

---
+++ directives/directiveform/src/app/style.directive.ts
@@ -0,0 +1,11 @@
+import { Directive ,ElementRef} from '@angular/core';
+
+@Directive({
+  selector: '[appStyle]'
+})
+export class StyleDirective {
+  constructor(private el:ElementRef) { 
+    el.nativeElement.style.color="green"
+  }
+
+}
